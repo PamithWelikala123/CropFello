88138f7b916d5e230d1076fc166d0ff500c4e215
K.D Rushin Sandeepana <rushinsandeepane11@gmail.com>
2023-02-20 17:17:23 +0530

    delivery UIs completed

--- app/view/deliverydone.view.php
+++ app/view/deliverydone.view.php
@@ -1,23 +1,22 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
-    <link rel="stylesheet" href="<?=ROOT?>/assets/css/deliverytodo.css">
+    <link rel="stylesheet" href="<?=ROOT?>/assets/css/deliverydone.css">
 </head>
 <body>
     <div class="container">
         <div class="item item1">
             <img class="logo" src="<?=ROOT?>/assets/images/rushinpics/pics/logo.png">
             <input class="Search" type="textbox" placeholder="Search">
-            <img class="Search1"  src="<?=ROOT?>/assets/images/rushinpics/pics/feed/search.png">
-            <img class="bell" src="<?=ROOT?>/assets/images/rushinpics/pics/feed/bell.png">
-            <img class="active" src="<?=ROOT?>/assets/images/rushinpics/pics/feed\active.png"> 
-            <img class="profile" src="<?=ROOT?>/assets/images/rushinpics/pics/feed\profilepic.png"> 
+            <img class="Search1"  src="<?=ROOT?>/assets/images/rushinpics/pics\feed\search.png">
+            <img class="bell" src="<?=ROOT?>/assets/images/rushinpics/pics\feed\bell.png">
+            <img class="active" src="<?=ROOT?>/assets/images/rushinpics/pics\feed\active.png"> 
+            <img class="profile" src="<?=ROOT?>/assets/images/rushinpics/pics\feed\profilepic.png"> 
         </div>
         <div class="item item2">
-          <img class="line2"  src="<?=ROOT?>/assets/images/rushinpics/pics/feed\line.png">
+          <img class="line2"  src="<?=ROOT?>/assets/images/rushinpics/pics\feed\line.png">
           <label class="Menu">Menu</label>
 
             <a href="todo.php"><button class="Feed">To Do</button></a>
@@ -29,7 +28,7 @@
             <a href="fee.php"><button class="Requests">Fee</button></a>
             <img class="Requests1"  src="<?=ROOT?>/assets/images/rushinpics/pics/feed/fee.png">
 
-            <img class="line1"  src="<?=ROOT?>/assets/images/rushinpics/pics/feed\line.png">
+            <img class="line1"  src="<?=ROOT?>/assets/images/rushinpics/pics\feed\line.png">
             <label class="Message">Message</label>
 
             <div class="box" style="background-color:#FFFFFF; width: 130px; height:100px; border: 1px solid rgb(16, 17, 16); padding: 40px; margin-top: 370px;">
@@ -50,13 +49,13 @@
         <div class="item item3">
             <div class="scroll-div">
             <?php
-                    $sql = "SELECT * FROM info WHERE status=0";
+                    $sql = "SELECT * FROM info WHERE status=2";
                     $result = $conn->query($sql);
             if ($result->num_rows > 0) { ?>
                     <?php while ($row = $result->fetch_assoc()) { ?>
                 <div class="containerx">
                     <div class="item item-1">
-                      <img class="profile-pic" src="<?=ROOT?>/assets/images/rushinpics/pics/man1.png">
+                      <img class="profile-pic" src="<?=ROOT?>/assets/images/rushinpics/pics/man3.png">
                       <p class="nam"><?php echo  $row['buyer'] ?></p><br>
                       <p class="date"><?php echo date('d F y', strtotime($row['date'])) ?></p>
 
@@ -99,10 +98,9 @@
                         </tr>
                       </table>
 
-                      <a href='todo.php?id=<?php echo $row['id'] ?>'><button class="Approve btn1">Approve &nbsp  &#10004</button></a>
-                      <a href='todo.php?eid=<?php echo $row['id'] ?>'><button class="Discard btn2">Discard &nbsp &#xf014 </button></a>
+                      <a href='todo.php?id=<?php echo $row['id'] ?>'<button class="Approve btn1">Done &nbsp  &#10004</button></a>
+                      <a href='todo.php?eid=<?php echo $row['id'] ?>'<button class="Discard btn2">Discard &nbsp &#xf014 </button></a>
                     </div>
-              
                   </div>                
                  <?php }?>
                  <?php }?>
