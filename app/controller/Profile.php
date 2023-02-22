17cff9ba9753f8b666242dd9b0061a5f3745db2b
PamithWelikala123 <108875335+PamithWelikala123@users.noreply.github.com>
2023-02-23 03:34:52 +0530

    editing profile edit

--- app/controller/Profile.php
+++ app/controller/Profile.php
@@ -1,10 +1,16 @@
 <?php
 
 //Editprofile_changepassword class
-class Editprofile{
+class profile{
 
     use Controller;
     public function index(){
+
+        
+    }
+
+    public function Editprofile_details(){
+
 
         $this->view('Editprofile');
     }
