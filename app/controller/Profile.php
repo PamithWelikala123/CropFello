caa95004370100210f53706138eacd3d67841415
PamithWelikala123 <108875335+PamithWelikala123@users.noreply.github.com>
2023-02-24 11:08:16 +0530

    editing edit profile php for 24-feb

--- app/controller/Profile.php
+++ app/controller/Profile.php
@@ -16,7 +16,74 @@ class profile{
         $row=$user->first($arr);
         $row=(array)$row;
 
+
+
+
         $this->view('Editprofile',$row);
+    }
+
+
+    public function editprofile(){
+      $user= new User;
+
+            if(isset($_POST["submit"])){
+            
+                  if($_FILES["image"]["error"] == 4){
+                    echo
+                    "<script> alert('Image Does Not Exist'); </script>"
+                    ;
+                  }
+                  else{
+                    $fileName = $_FILES["image"]["name"];
+                    $fileSize = $_FILES["image"]["size"];
+                    $tmpName = $_FILES["image"]["tmp_name"];
+                
+                    $validImageExtension = ['jpg', 'jpeg', 'png'];
+                    $imageExtension = explode('.',$fileName);
+                    $imageExtension = strtolower(end($imageExtension));
+                
+                    $newImageName = uniqid();
+                    $newImageName .= '.'.$imageExtension;
+
+                    $destinationPath = $_SERVER['DOCUMENT_ROOT'] . '/Cropfello/public/assets/images/Profile_pic/'.$newImageName;
+                  //  echo $newImageName;
+                    move_uploaded_file($tmpName, $destinationPath); 
+                    $arr['image']=$newImageName;
+                   // $user->update($_SESSION['USER']->user_id,$arr ,'user_id');
+                  }
+                
+              }
+            $arr['user_id']=$_SESSION['USER']->user_id;
+            $row=$user->first($arr);
+            $row=(array)$row;
+
+            if(($row['first_name'])!=$_POST['first_name']){
+              $arr['first_name']=$_POST['first_name'];
+            }
+            
+            if(($row['last_name'])!=$_POST['last_name']){
+              $arr['last_name']=$_POST['last_name'];
+            }
+
+            if(($row['address'])!=$_POST['address']){
+              $arr['address']=$_POST['address'];
+            }
+            
+            if(($row['city'])!=$_POST['city']){
+              $arr['city']=$_POST['city'];
+            }
+
+            if(($row['description'])!=$_POST['description']){
+              $arr['description']=$_POST['description'];
+            }
+
+
+            $user->update($_SESSION['USER']->user_id,$arr ,'user_id');
+
+
+
+
+
     }
 
 }
--- app/view/editprofile.view.php
+++ app/view/editprofile.view.php
@@ -18,7 +18,7 @@
         <div class="Register">
               <p class="letter">Edit Profile</p>
         </div>
-
+    <form class="" action="editprofile" method="post" enctype="multipart/form-data">
         <div style="display : grid; grid-template-columns: 50% 50%">
               <div>
 
@@ -27,10 +27,10 @@
                               <div>
                                 <label class="name">Name</label>
                                 <label class="name1">*</label>
-                                <input class="firstname" type="textbox" placeholder="  <?=$data['first_name']?>"> 
+                                <input class="firstname" type="textbox" placeholder="<?=$data['first_name']?>" name="first_name"> 
                               </div>
                               <div>
-                                <input class="lastname" type="textbox" placeholder="  <?=$data['last_name']?>"> 
+                                <input class="lastname" type="textbox" placeholder="<?=$data['last_name']?>" name="last_name"> 
                               </div>
 
                      </div>
@@ -39,7 +39,7 @@
                         <label class="Address-name" >Address</Address></label>
                         <label class="Address-name1">*</label>
                         <br>
-                           <input class="Address" type="textbox" placeholder="  <?=$data['address']?>"> 
+                           <input class="Address" type="textbox" placeholder="<?=$data['address']?>" name="address"> 
                      </div>
                      
                      <div style="display : grid; grid-template-columns: 50% 50%">
@@ -48,14 +48,14 @@
                               <label class="Postal-code-name">Postal code</Address></label>
                               <label class="Postal-code-name1">*</label>
                               <br>
-                              <input class="Postal-code" type="textbox" placeholder="  <?=$data['postal_code']?>"> 
+                              <input class="Postal-code" type="textbox" placeholder="<?=$data['postal_code']?>" name="postal_code"> 
                             </div>
 
                             <div>
                               <label class="City-name">City</Address></label>
                               <label class="City-name1">*</label>
                               <br>
-                              <input class="City" type="textbox" placeholder="  <?=$data['city']?>">
+                              <input class="City" type="textbox" placeholder="<?=$data['city']?>" name="city"> 
                             </div>
                      </div>
 
@@ -66,7 +66,7 @@
                              <img class="srilanka"  src="<?=ROOT?>/assets/images/buyersignup page/Sri Lanka.png">
                              <label class="ContactNo-name2">|</label>
                              <label class="ContactNo-name3">+94</label>
-                            <input class="ContactNo" type="textbox" placeholder="  <?=$data['contact_number']?>">
+                            <input class="ContactNo" type="textbox" placeholder="<?=$data['contact_number']?>" name="contact_number">
                      </div>
 
 
@@ -85,9 +85,10 @@
 
                      <div>
                      <img class="img5" src="<?=ROOT?>/assets/images/editprofile/drop-image.svg">
+                     <input class="file" type="file" name="image" id = "image" accept=".jpg, .jpeg, .png" value=""> 
                      
                      </div>
-                     <input class="about" type="textbox" placeholder="  Discribe yourself...">
+                     <input class="about" type="textbox" placeholder="  Discribe yourself..." name="description">
                      <div>
 
                      </div>
@@ -98,13 +99,13 @@
                       </div>
 
                       <div>
-                          <button class="Register-but">Update</button> 
+                          <button class="Register-but" type="submit" name="submit">Update</button> 
                       </div>
-
+               
 
             </div>
         </div>
-
+        </form>
         <div>
           <img class="img2" src="<?=ROOT?>/assets/images/editprofile/register-corner2.png">
           <img class="img3" src="<?=ROOT?>/assets/images/editprofile/register-corner.png">
