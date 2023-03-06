bfb9a0a717109c0afed08757e9f4c54a0be486ad
PamithWelikala123 <108875335+PamithWelikala123@users.noreply.github.com>
2023-03-06 11:00:20 +0530

    code backend of Dashboard

--- app/view/home.view.php
+++ app/view/home.view.php
@@ -31,15 +31,15 @@
             <div class="left">
                 <div class="buyer">
                     <h6>Buyers</h6>
-                    <h4>460</h4>
+                    <h4><?=$data['buyercount']?></h4>
                 </div>
                 <div class="seller">
                     <h6>Sellers</h6>
-                    <h4>345</h4>
+                    <h4><?=$data['sellercount']?></h4>
                 </div>
                 <div class="deliverer">
                     <h6>Delivers</h6>
-                    <h4>118</h4>
+                    <h4><?=$data['deliverycount']?></h4>
                 </div>
                 <div class="this-month"></div>
             </div>
@@ -61,101 +61,29 @@
             
 
         </div>
+
         <div class="offers">
             <div class="heading">
                 <h2>Best Offers</h2>
             </div>
-            <div class="responsive1">
+            <?php while ($data['count'] > 0) :?>
+            <div class="responsive<?php$data['count']?>">
                 <div class="gallery">
                   <a target="_blank" href="img_5terre.jpg">
-                    <img src="<?=ROOT?>/assets/images/home/carrot1.jpg" alt="img1" width="600" height="400">
+                    <img src="<?=ROOT?>/assets/images/Post-images/<?=$data['image'.$data['count']]?>" alt="img1" width="600" height="400">
                   </a>
                   <div class="desc">
-                    <p style="font-weight: bold;">Carrot <span style="float: right; opacity: 80%;">RS. 130.00</span></p>
-                    <p style="color: #0EBC1F;">1 KG <span style="float: right; color: #FB7A7A;">70% OFF</span></p>
+                    <p style="font-weight: bold;"> <?php echo $data['item_name'.$data['count']] ?><span style="float: right; opacity: 80%;">RS. <?php echo $data['price'.$data['count']] ?></span></p>
+                    <p style="color: #0EBC1F;"><?php echo $data['size'.$data['count']]?> KG <span style="float: right; color: #FB7A7A;"><?php echo $data['discount'.$data['count']]?>% OFF</span></p>
                 </div>
                 </div>
-              </div>
-              
-              
-              <div class="responsive2">
-                <div class="gallery">
-                  <a target="_blank" href="img_forest.jpg">
-                    <img src="<?=ROOT?>/assets/images/home/pinaple1.jpg" alt="img2" width="600" height="400">
-                  </a>
-                  <div class="desc">
-                    <p style="font-weight: bold;">Pineapple <span style="float: right; opacity: 80%;">RS. 350.00</span></p>
-                    <p style="color: #0EBC1F;">1 KG <span style="float: right; color: #FB7A7A;">60% OFF</span></p>
-                  </div>
-                </div>
-              </div>
-              
-              <div class="responsive3">
-                <div class="gallery">
-                  <a target="_blank" href="img_lights.jpg">
-                    <img src="<?=ROOT?>/assets/images/home/green-beans-bonchi.jpg" alt="img3" width="600" height="400">
-                  </a>
-                  <div class="desc">
-                    <p style="font-weight: bold;">Beens <span style="float: right; opacity: 80%;">RS. 460.00</span></p>
-                    <p style="color: #0EBC1F;">1 KG <span style="float: right; color: #FB7A7A;">50% OFF</span></p>
-                  </div>
-                </div>
-              </div>
-              
-              <div class="responsive4">
-                <div class="gallery">
-                  <a target="_blank" href="img_mountains.jpg">
-                    <img src="<?=ROOT?>/assets/images/home/tomato.jpeg" alt="img4" width="600" height="400">
-                  </a>
-                  <div class="desc">
-                    <p style="font-weight: bold;">Tomato <span style="float: right; opacity: 80%;">RS. 399.00</span></p>
-                    <p style="color: #0EBC1F;">1 KG <span style="float: right; color: #FB7A7A;">50% OFF</span></p>
-                  </div>
-                </div>
-              </div>
-        </div>
-        <!-- <div class="featured">
-            <div class="heading">
-                <h2>Featured</h2>
             </div>
-            <div class="responsive">
-                <div class="gallery">
-                  <a target="_blank" href="img_5terre.jpg">
-                    <img src="img_5terre.jpg" alt="Cinque Terre" width="600" height="400">
-                  </a>
-                  <div class="desc">Add a description of the image here</div>
-                </div>
-              </div>
+            <?php $data['count']=$data['count']-1;?>
+            <?php endwhile;?>
               
               
-              <div class="responsive">
-                <div class="gallery">
-                  <a target="_blank" href="img_forest.jpg">
-                    <img src="img_forest.jpg" alt="Forest" width="600" height="400">
-                  </a>
-                  <div class="desc">Add a description of the image here</div>
-                </div>
-              </div>
-              
-              <div class="responsive">
-                <div class="gallery">
-                  <a target="_blank" href="img_lights.jpg">
-                    <img src="img_lights.jpg" alt="Northern Lights" width="600" height="400">
-                  </a>
-                  <div class="desc">Add a description of the image here</div>
-                </div>
-              </div>
-              
-              <div class="responsive">
-                <div class="gallery">
-                  <a target="_blank" href="img_mountains.jpg">
-                    <img src="img_mountains.jpg" alt="Mountains" width="600" height="400">
-                  </a>
-                  <div class="desc">Add a description of the image here</div>
-                </div>
-              </div>          
-        </div> -->
-        
+        </div>
+
     </div>
 </body>
 </html>
