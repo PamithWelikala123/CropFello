f616da063df13234ba73ab94c495c73a94d7d482
vimukthi00 <vimukthidulnath@gmail.com>
2023-04-13 17:46:41 +0530

    seperate topnav and leftbar css

--- app/view/buyerbidding2.view.php
+++ app/view/buyerbidding2.view.php
@@ -9,6 +9,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?=ROOT?>/assets/css/buyerbidding2.css">
+    <link rel="stylesheet" href="<?=ROOT?>/assets/css/buyerleftbar.css">
+    <link rel="stylesheet" href="<?=ROOT?>/assets/css/buyertopnav.css">
 </head>
 <body>
 
