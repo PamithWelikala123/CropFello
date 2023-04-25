3dac1e6da76a159b8e07ddaf8bb06361a90116f4
PamithWelikala123 <108875335+PamithWelikala123@users.noreply.github.com>
2023-04-25 13:13:04 +0530

    created new edit profile view

--- app/view/viewownprofile.view.php
+++ app/view/viewownprofile.view.php
@@ -12,9 +12,7 @@
 </head>
 <body>
 
-
-
-    <div class="container">
+<div class="container">
             <?php require APPROOT."/view/searchbarbuyer2.php"?>
 
         <div class="content">
@@ -25,64 +23,66 @@
                 <a href="#contact"><i class="fa fa-flag-o"></i> Requests</a> -->
 
                 <div class="menu">
-                  
+
                   <hr><p>Menu</p><br>
-                  <button onclick="document.location='../feed/feed'"><img class="Feed1"  src="<?=ROOT?>/assets/images/feed/feed1.png"><span> Feed</span></button><br>
-                  <button onclick="document.location='../bidding/BuyerBidding'"><img class="Bidding1"  src="<?=ROOT?>/assets/images/feed/Bidding1.png"><span> Bidding</span></button><br>
-                  <button onclick="document.location='../wishlists/buyerwishlist'"><img class="Bidding1"  src="<?=ROOT?>/assets/images/feed/heart1.png"><span> Wishlist</span></button><br>
-                  <button onclick="document.location='../requests/view1'"><img class="Requests1"  src="<?=ROOT?>/assets/images/feed/flag.png"><span> Requests</span></button><br>
+                  <button onclick="document.location='feed/feed'"><img class="Feed1"  src="<?=ROOT?>/assets/images/feed/feed1.png"><span> Feed</span></button><br>
+                  <button onclick="document.location='bidding/BuyerBidding'"><img class="Bidding1"  src="<?=ROOT?>/assets/images/feed/Bidding1.png"><span> Bidding</span></button><br>
+                  <button onclick="document.location='wishlists/buyerwishlist'"><img class="Bidding1"  src="<?=ROOT?>/assets/images/feed/heart1.png"><span> Wishlist</span></button><br>
+                  <button onclick="document.location='requests/view1'"><img class="Requests1"  src="<?=ROOT?>/assets/images/feed/flag.png"><span> Requests</span></button><br>
                 </div>
+
                 <div class="message">
-                    
-                          <hr><p>Message</p><br>
-                          <!-- <img class="message-bar" src="images/message-bar.png"> -->
-                          <div class="message-box">
-                              <?php require APPROOT."/controller/Chat.php";
-                              $userController = new Chat();
-                              $userController->viewperson();
-                              ?>
-                                  
-                          
 
-                          
+<hr><p>Message</p><br>
+<!-- <img class="message-bar" src="images/message-bar.png"> -->
+<div class="message-box">
+    <?php require APPROOT."/controller/Chat.php";
+    $userController = new Chat();
+    $userController->viewperson();
+    ?>
 
-                          </div>
-                </div>
+
+
+
+
 </div>
-            <div class="main">
-                  <div class="main-content">
-                                  <div class="main-viewprofile">
-                                      <div class="dp-viewprofile">
-                                        <img src="<?=ROOT?>/assets/images/Profile_pic/<?=$_SESSION['USER']->image?>" alt="dp">
-                                      </div>
-                                      <div class="account-details-viewprofile">
-                                        <div class="name-viewprofile">
-                                          <h2><?=$data['first_name']?> <?=$data['last_name']?></h2>
-                                        </div>
-                                  
-                                        <div class="empty-viewprofile"></div>
-                                        
-                                        <div class="contact-viewprofile">
-                                          <label for=""><img src="<?=ROOT?>/assets/images/images/call.svg" alt="">&nbsp;+94 <?=$data['contact_number']?> &emsp;&emsp;</label><label for=""><img src="<?=ROOT?>/assets/images/images/mail.svg" alt="">&nbsp;<?=$data['email']?></label>
-                                        </div>
-                                        <div class="messages-viewprofile">
-                                          <button onclick="document.location='Profile/Editprofile_details'">Edit</button>
-                                        </div>
-                                      </div>
-                                      <div class="about-viewprofile">
-                                        <h1>About</h1>
-                                        <p><?=$data['description']?></p>
-                                      </div>
-                                    </div>
-                              
-                    </div>
+</div>
+</div>
+<div class="main">
+<div class="main-content">
+        <div class="main-viewprofile">
+            <div class="dp-viewprofile">
+              <img src="<?=ROOT?>/assets/images/Profile_pic/<?=$_SESSION['USER']->image?>" alt="dp">
+            </div>
+            <div class="account-details-viewprofile">
+              <div class="name-viewprofile">
+                <h2><?=$data['first_name']?> <?=$data['last_name']?></h2>
               </div>
-        
-    </div>
-    
-    
+
+              <div class="empty-viewprofile"></div>
+
+              <div class="contact-viewprofile">
+                <label for=""><img src="<?=ROOT?>/assets/images/images/call.svg" alt="">&nbsp;+94 <?=$data['contact_number']?> &emsp;&emsp;</label><label for=""><img src="<?=ROOT?>/assets/images/images/mail.svg" alt="">&nbsp;<?=$data['email']?></label>
+              </div>
+              <div class="messages-viewprofile">
+                <button onclick="document.location='Profile/Editprofile_details'">Edit</button>
+              </div>
+            </div>
+            <div class="about-viewprofile">
+              <h1>About</h1>
+              <p><?=$data['description']?></p>
+            </div>
+          </div>
+
+</div>
+</div>
+
+
+
+</div>
+
+
 
 <!-- Modal Section -->
 
   </body>
-</html>
