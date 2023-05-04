18d863005cb0ce136bd0c461bb6ee51d4c9a3fac
K.D Rushin Sandeepana <rushinsandeepane11@gmail.com>
2023-05-04 10:47:33 +0530

    new things8

--- app/view/deliverylogin.view.php
+++ app/view/deliverylogin.view.php
@@ -8,7 +8,7 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-        <link rel="Stylesheet" href="<?=ROOT?>/assets/css/login.css">
+        <link rel="Stylesheet" href="<?=ROOT?>/assets/css/deliverylogin1.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
         
     </head>
@@ -17,15 +17,15 @@
 
         <div class="back">
                 <div class="mainletter">
-                          <p class="letters">Don't have an account? <a class="para">Register as a</a></p>
-                          <a href="<?=ROOT?>/buyersignup">
-                          <button class="button-buyer">Buyer</button>
+                          <p class="letters">Don't have an account? <a class="para">Register as a Deliverer</a></p>
+                          <!-- <a href="<?=ROOT?>/buyersignup">
+                          <button class="button-buyer">Buyer</button> -->
                          
-                          <a href="<?=ROOT?>/sellersignup">
-                          <button class="button-seller">Seller</button>
-                          </a>
-
-                          <button class="button-deliverer">Deliverer</button>
+                          <!-- <a href="<?=ROOT?>/sellersignup">
+                          <button class="button-seller">Seller</button> 
+                          </a> -->
+                          <a href="<?=ROOT?>/deliverysignup">
+                          <!-- <button class="button-deliverer">Deliverer</button></a> -->
                           
                </div>
             
