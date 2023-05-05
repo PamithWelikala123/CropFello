80bdcfd429f7d4b8350fbfd7a07af85bd3f424bd
Pamith Welikala <pamithwelikala@gmail.com>
2023-05-05 07:37:05 +0530

    new commit

--- app/view/sellersignup.view.php
+++ app/view/sellersignup.view.php
@@ -7,148 +7,149 @@
         </title>
         <link rel="Stylesheet" href="<?=ROOT?>/assets/css/buyersignup.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+        <script type="text/javascript" src="<?php echo AUTO_MAP_URL ?>" defer></script>
+        <script src="<?=ROOT?>/assets/js/map.js"></script> 
         
     </head>
-    <body>
+    
+<body onload="initMap()">
 
-        <div class="logo">
-               <img class="img1"  src="<?=ROOT?>/assets/images/buyersignup page/logo.png">
-        </div>
-        <img  class="img4" src="<?=ROOT?>/assets/images/editprofile/arrow-left-circle.svg" onclick="document.location='login'">
-        <div class="Register">
-              <p class="letter">Register to CropFello</p>
-        </div>
+<img src="http://localhost/Cropfello/vimukthi/register form/images/logo.png" alt="">
+<p class="login">Login</p>
+<div class="row">
+  <div class="col-75">
+    <div class="container">
+      <form method="post">
+      
+        <div class="row">
+          <div class="col-50">
+            <div class="row">
+                <div class="col-50">
+                  <label for="name">Name</label>
+                  <?php if(empty($errors['first_name'])):?>
+                  <input type="text" id="firstname"  placeholder="First Name" name="first_name">
+                  <?php else:?>
+                    <input style="border: 1px solid #FB7A7A;" type="text" id="firstname" name="first_name">
+                  <?php endif;?>
+                </div>
 
-        <div style="display : grid; grid-template-columns: 60% 40%">
-              <div>
-         <form method="post">
-
-        <div style="display : grid; grid-template-columns: 50% 50%"> 
-         <div>
-         <?php if(!empty($errors['first_name'])):?>
-          <input class="firstname1" type="text" placeholder="  First Name" name="first_name">
-         <?php else:?>
-          <input class="firstname" type="text" placeholder="  First Name" name="first_name">
-         <?php endif;?>
+                <div class="col-50">
+                
+                  <label for="lastname">&nbsp;</label>
+                  <?php if(empty($errors['last_name'])):?>
+                    <input type="text" id="lastname" name="last_name" placeholder="Last Name">
+                  <?php else:?>
+                  <input style="border: 1px solid #FB7A7A;" type="text" id="lastname" name="last_name" placeholder="Last Name">
+                  <?php endif;?>
+                </div>
+              </div>
+        
+            <label for="adr">Address</label>
+                  <?php if(empty($errors['address'])):?>
+                          <input type="text" id="adress" name="address">
+                  <?php else:?>
+                        <input style="border: 1px solid #FB7A7A;" type="text" id="adress" name="address">
+                  <?php endif;?>
             
-         </div>
-         <div>
-         <?php if(!empty($errors['last_name'])):?>
-          <input class="lastname1" type="text" placeholder="  Last Name" name="last_name"> 
-         <?php else:?>
-          <input class="lastname" type="text" placeholder="  Last Name" name="last_name"> 
-         <?php endif;?>
-           
-         </div>
 
-</div>
+            <div class="row">
+              <div class="col-25">
+                
+                <label for="city">City</label>
+                      <?php if(empty($errors['city'])):?>
+                      <input type="text" id="city" name="city">
+                      <?php else:?>
+                        <input style="border: 1px solid #FB7A7A;" type="text" id="city" name="city">
+                      <?php endif;?>
+                
+                    </div>
 
-<div>
-   <label class="Address-name">Address</Address></label>
+                    <div class="col-25">
+                    <label for="contact">Contact No</label>
+                    <?php if(empty($errors['address'])):?>
 
-   <?php if(!empty($errors['address'])):?>
-         <label class="Address-name1-1">*</label>
-         <?php else:?>
-         <label class="Address-name1">*</label>
-         <?php endif;?>
-   <br>
-      <input class="Address" type="text" name="address"> 
-</div>
-
-<div style="display : grid; grid-template-columns: 50% 50%">
- 
-       <div> 
-         <label class="Postal-code-name">Postal code</Address></label>
-         
-         <?php if(!empty($errors['postal_code'])):?>
-         <label class="Postal-code-name1-1">*</label>
-         <?php else:?>
-         <label class="Postal-code-name1">*</label>
-         <?php endif;?>
-         
-         <br>
-         <input class="Postal-code" type="text" name="postal_code"> 
-       </div>
-
-       <div>
-         <label class="City-name">City</Address></label>
-         <?php if(!empty($errors['city'])):?>
-         <label class="City-name1-1">*</label>
-         <?php else:?>
-         <label class="City-name1">*</label>
-         <?php endif;?>
-         <br>
-         <input class="City" type="text" name="city">
-       </div>
-</div>
-
-<div>
-      <label class="ContactNo-name">Contact No</Address></label>
-    <?php if(!empty($errors['contact_number'])):?>
-        <label class="ContactNo-name1-1">*</label>
-    <?php else:?>
-      <label class="ContactNo-name1">*</label>
-    <?php endif;?>
-        <br>
-        <img class="srilanka"  src="<?=ROOT?>/assets/images/buyersignup page/Sri Lanka.png">
-        <label class="ContactNo-name2">|</label>
-        <label class="ContactNo-name3">+94</label>
-       <input class="ContactNo" type="text" name="contact_number">
-</div>
-
-
-</div>
-<div>
- <div>
- 
-   <label class="Email-name">Email</label>
-   <?php if(!empty($errors['email'])):?>
-   <label class="Email-name1-1">*</label>
-   <?php else:?>
-    <label class="Email-name1">*</label>
-    <?php endif;?>
-   <br>
-   <input class="Email" type="email" name="email">
- </div>
-
- <div>
-   <label class="Password-name">Passsword</label>
-   <?php if(!empty($errors['password'])):?>
-   <label class="Password-name1-1">*</label>
-   <?php else:?>
-    <label class="Password-name1">*</label>
-    <?php endif;?>
-   <br>
-   <input class="Password" type="password" name="password">
-   <i class="fas fa-eye"></i>
- </div>
-
- <div>
-   <label class="Confirm-Password-name">Confirm Passsword</label>
-   <?php if(!empty($errors['password'])):?>
-   <label class="Confirm-Password-name1-1">*</label>
-   <?php else:?>
-    <label class="Confirm-Password-name1">*</label>
-    <?php endif;?>
-   <br>
-   <input class="Confirm-Password" type="password" name="confirm_password">
- </div>
-
- <div>
-     <input id="button" class="Register-but" type="submit" value="Register">
-     
- </div>
-</form>
-
+                      <input type="text" id="contact" name="contact_number">
+                      <?php else:?>
+                        <input style="border: 1px solid #FB7A7A;" type="text" id="contact" name="contact_number">
+                        <?php endif;?>
+                    </div>
+              
             </div>
+
+            <div class="row">
+                      <div class="col-25">
+                      <?php if(!empty($errors['city'])):?>
+                        <P><?php echo $errors['city'] ?></P>
+                      <?php endif;?>
+                      </div>
+
+                      <div class="col-25">
+                      <?php if(!empty($errors['contact_number'])):?>
+                        <P><?php echo $errors['contact_number'] ?></P>
+                      <?php endif;?>
+                      </div>
+          </div>  
+          </div>
+          
+
+          <div class="col-50">
+
+            <label for="">Location</label>
+            <?php if(empty($errors['pickuplocation'])):?>
+            <input type="text" id="pickuplocation" name="pickuplocation" placeholder="Select and fetch your location" value="" >
+            <?php else:?>
+              <input style="border: 1px solid #FB7A7A;" type="text" id="pickuplocation" name="pickuplocation" placeholder="Select and fetch your location" value="" >
+            <?php endif;?>
+            
+            
+
+            <input type="hidden" name="p-latitude" id="p-latitude" value="">
+            <input type="hidden" name="p-longitude" id="p-longitude" value="">
+            <input type="hidden" id="city1" name="city1" value="">
+<!-- 
+            <input type="text" id="destination" name="destination" placeholder="From Where journey End...?"  value=""> -->
+<!-- 
+            <input type="hidden" name="d-latitude" id="d-latitude" value="">
+            <input type="hidden" name="d-longitude" id="d-longitude" value="">
+
+
+
+            <input type="hidden" name="distance" id="distance" value="">
+            <input type="hidden" name="duration" id="duration" value=""> -->
+
+
+
+            <div class="map-container">
+                <div id="map"></div>
+            </div>
+
+
+            <div class="row">
+                <div class="col-25">
+                    <!-- <input type="submit" value="Next" class="btn" > -->
+                    <button type="submit" class="btn"><span>Next</span></i></button>
+                  </div>
+                
+            </div>
+          </div>
+          
         </div>
 
-        <div>
-          <img class="img2" src="<?=ROOT?>/assets/images/buyersignup page/register-corner2.png">
-          <img class="img3" src="<?=ROOT?>/assets/images/buyersignup page/register-corner.png">
-        </div>
+        
+      </form>
+    </div>
+  </div>
+
+</div>
+<div class="footer">
+    <img class="img2" src="http://localhost/Cropfello/vimukthi/register form/images/register-corner2.png" alt="">
+    <img class="img3" src="http://localhost/Cropfello/vimukthi/register form/images/register-corner.png" alt="">
+</div>
 
 
-    </body>
+
+
+
+</body>
     <script src="<?=ROOT?>/assets/js/pass-show-hide.js"></script>
 </html>
