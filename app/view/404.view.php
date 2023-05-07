ff4610f78272f5198eb344cd812062fc3079eb6a
vimukthi00 <vimukthidulnath@gmail.com>
2023-05-08 01:09:48 +0530

    something went wrong page

--- app/view/404.view.php
+++ app/view/404.view.php
@@ -1,1 +1,15 @@
-<h1>view file not found(praneeth)</h1>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Something Went Wrong</title>
+  <link rel="Stylesheet" href="<?=ROOT?>/assets/css/404.css">
+</head>
+<body>
+  <div class="error-container">
+    <img src="<?=ROOT?>/assets/images/logo.png" alt="">
+    <h1><span class="oops">Oops!</span>  Something Went Wrong</h1>
+    <p>We're sorry, but something went wrong. Please try again later.</p>
+    <a href="index.html">Go back to the homepage</a>
+  </div>
+</body>
+</html>
