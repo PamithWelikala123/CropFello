8dc2d21d252c0424e9cd35ce6d7e8ba84c26bdbb
praneeth <praneeth495@gmail.com>
2023-05-07 21:36:30 +0530

    add buying proccess

--- app/view/cart.view.php
+++ app/view/cart.view.php
@@ -1,88 +1,203 @@
+
 <!DOCTYPE html>
-<html>
-  <head>
-    <title>
-      Cart
-    </title>
-    <link rel="Stylesheet" href="<?=ROOT?>/assets/css/cart.css">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-    <style>
-     
-      </style>
-  </head>
-  <body>
-    <div style="display : grid; grid-template-columns: 21% 79%;">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>navigation</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"-->
+    <link rel="stylesheet" href="<?=ROOT?>/assets/css/cart.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="topnav">
+            <div class="logo">
+                <img src="<?=ROOT?>/assets/images/cart/logo.png" altba="">
+            </div>
+            <div class="topnav-options">
+                <!-- <a class="active" href="#home">Home</a> -->
+                <a href="#orders" id="my-orders">My Orders</a>
+                <a href="#cart"><i class="fa fa-shopping-cart" style="font-size:18px"></i></a>
+                <a href="#notifi"><i class="fa fa-bell" style="font-size:18px"></i></a>
+                <!-- <a href="#profile"> -->
+                    <img src="<?=ROOT?>/assets/images/cart/user.jpg" alt=""> 
+                <!-- </a> -->
+            </div>
+                <div class="search-container">
+                  <form action="/action_page.php">
+                    <input type="text" placeholder="Search.." name="search">
+                    <button type="submit"><i class="fa fa-search"></i></button>
+                  </form>
+                </div>
+          </div>
+        <div class="content">
+            <div class="leftbar">
+                <!-- <a href="#home"><i class="fa fa-rss"></i> Feed</a>
+                <a href="#services"><i class="fa fa-dollar"></i> Bidding</a>
+                <a href="#clients"><i class="fa fa-bookmark-o"></i> Wishlist</a>
+                <a href="#contact"><i class="fa fa-flag-o"></i> Requests</a> -->
 
-          <div class="left-line"> 
-                    <img class="Menu"  src="<?=ROOT?>/assets/images/feed/left.png">
-                    <img class="logo"  src="<?=ROOT?>/assets/images/feed/logo.png">
-                    <img class="line"  src="<?=ROOT?>/assets/images/feed/line.png">
-                    <img class="message-bar" src="<?=ROOT?>/assets/images/feed/message-bar.png">
-                    <label class="Menu1">Menu</label>
-                    
+                <div class="menu">
+                  <!-- <hr><label >Menu</label><br> -->
+                  <hr><p>Menu</p><br>
+                  <button><img class="Feed1"  src="<?=ROOT?>/assets/images/cart/feed1.png"><span> Feed</span></button><br>
+                  <button><img class="Bidding1"  src="<?=ROOT?>/assets/images/cart/Bidding1.png"><span> Bidding</span></button><br>
+                  <button><img class="Bidding1"  src="<?=ROOT?>/assets/images/cart/wishlist1.png"><span> Wishlist</span></button><br>
+                  <button><img class="Requests1"  src="<?=ROOT?>/assets/images/cart/flag.png"><span> Requests</span></button><br>
+              </div>
+              <div class="message">
+                  <!-- <hr><label>Message</label> -->
+                  <hr><p>Message</p><br>
+                  <!-- <img class="message-bar" src="images/message-bar.png"> -->
+                  <div class="message-box">
 
-                    <div> 
-                      <button class="Feed" onclick="document.location='feed'">Feed</button>
-                      <img class="Feed1"  src="<?=ROOT?>/assets/images/feed/feed1.png">
-                      <button class="Bidding" onclick="document.location='buyerbidding'">Bidding</button>
-                      <img class="Bidding1"  src="<?=ROOT?>/assets/images/feed/Bidding1.png">
-                      <button class="Wishlist" onclick="document.location='buyerwishlist'" >Wishlist</button>
-                      <img class="Wishlist1"  src="<?=ROOT?>/assets/images/feed/heart1.png">
-                      <button class="Requests" id="Requests" onclick="document.location='buyerrequest'">Requests</button>
-                      <img class="Requests1"  src="<?=ROOT?>/assets/images/feed/flag.png">
+                    <div class="message0">
+                      <div class="message1">
+                        <img src="<?=ROOT?>/assets/images/cart/vimukthi.jpg" alt="">
+                      </div>
+                      <div class="message2">
+                        <label for="">Vimukthi Dulnath</label>
+                      </div>
                     </div>
 
-                    <img class="line1"  src="<?=ROOT?>/assets/images/feed/line.png">
-                    <label class="Message">Message</label>
+                    <div class="message0">
+                      <div class="message1">
+                        <img src="<?=ROOT?>/assets/images/cart/vimukthi.jpg" alt="">
+                      </div>
+                      <div class="message2">
+                        <label for="">Vimukthi Dulnath</label>
+                      </div>
+                    </div>
+
+                    <div class="message0">
+                      <div class="message1">
+                        <img src="<?=ROOT?>/assets/images/cart/vimukthi.jpg" alt="">
+                      </div>
+                      <div class="message2">
+                        <label for="">Vimukthi Dulnath</label>
+                      </div>
+                    </div>
+
+                    <div class="message0">
+                      <div class="message1">
+                        <img src="<?=ROOT?>/assets/images/cart/vimukthi.jpg" alt="">
+                      </div>
+                      <div class="message2">
+                        <label for="">Vimukthi Dulnath</label>
+                      </div>
+
+                    </div>
+
+                    <div class="message-footer">
+                      <button>All Messages</button>
+                    </div>
+
+                  </div>
+              </div>
+
+            </div>
+            <div class="main">
+                <!-- <div class="page-selector">
+                    <button class="active" onclick="window.location.href = 'index.html';">All</button>
+                    <button onclick="window.location.href = 'index2.html';">My Bids</button>
+                </div> -->
+                <div class="main-content">
+
+                  <div class="cart">
+                    <table class="cart-table">
+                    
+                      <thead>
+                        <tr>
+                          <th>Product</th>
+                          <th>Quantity</th>
+                          <th></th>
+                          <th>Price</th>
+                          <th></th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                      
+                        <tr>
+                        <?php foreach($data['a'] as $dat2){ ?>
+                          <td>
+                            <div class="product">
+                            <img src="<?=ROOT?>/assets/images/Post-images/<?php echo($dat2->image);  ?>" alt="Product Image">
+                              <span class="product-name"><br><span class="seller-name"> 
+                        <div><?php echo($dat2->item_name)?>  </div></span></span>
+                            </div>
+                          </td>
+                          <td>
+                            <span class="quantity-text"><?php echo($dat2->qua);  ?> <?php echo($dat2->unit);  ?></span>
+                          </td>
+                          <td>
+                            <!--div class="quantity">
+                              <button class="minus-btn" type="button">-</button>
+                              <input type="text" class="quantity-input" value="1">
+                              <button class="plus-btn" type="button">+</button>
+                            </div-->
+                          </td>
+                          <td>
+                            <span class="price">Rs. <?php echo($dat2->price);  ?></span>
+                          </td>
+                          <td><form method="post">
+                            <!--button class="checkout-btn" name="checkout">Checkout</button-->
+                            <?php echo '<button class="checkout-btn" name="btn['.$dat2->order_code.']/">Checkout</button>' ?>
+                            <?php echo '<button name="del['.$dat2->order_code.']/"><i class="fas fa-trash-alt"></i></button>' ?>
+                            <!--button name="del"><i class="fas fa-trash-alt"></i></button-->
+                            </form>
+                          </td>
+                        </tr>
+                        
+                      </tbody>
+                      <?php }
+                        ?>
+                    </table>
+                    <?php foreach($data['b'] as $dat){ ?>
+                    <div class="total-price">
+                      <span>Rs. <?php echo($dat->total);  ?></span>
+                    </div>
+                    <?php }
+                        ?>
+                  </div>
+                  
                   
 
-          </div>
-
-
-          <div style="display : grid; grid-template-rows: 10% 10% 80%; ;">
-                  <div class="top-line">
-                  <?php require APPROOT."/view/searchbarbuyer.php"?>
-          </div>
-          
-        <div class="pro">Product</div>
-        <div class="pro" style="left:621px">Quantity</div>
-        <div class="pro" style="left:821px">Units</div>
-        <div class="pro" style="left:1021px">Total</div>
-        <div><img class="Line5" src="<?=ROOT?>/assets/images/pics/Line5.png"></div>
-        <div class="bid">
-            <div class="bid1">
-                <div class="bid2">
-                    <div class="carr"><img class="img3"  src="<?=ROOT?>/assets/images/pics/carrot.jpg"></div><div>Carrot</div><div>10</div><div>KG</div><div class="box20" style="text-align:center;font-size:20px">- 2 +</div><div>Rs.600</div><div><button class="btn2" onclick="document.location='checkout'">Check out</button></div><div><img class="img1" src="<?=ROOT?>/assets/images/pics/delete.png"></div>
-                    <div style="color:#4DEA25">By Anura</div><div></div><div></div><div></div><div style="color:rgba(0, 0, 0, 0.4)">Rs.350</div>
-                </div>
-                <div class="bid2">
-                    <div class="carr"><img class="img3"  src="<?=ROOT?>/assets/images/pics/carrot.jpg"></div><div>Carrot</div><div>10</div><div>KG</div><div class="box20" style="text-align:center;font-size:20px">- 2 +</div><div>Rs.600</div><div><button class="btn2" onclick="document.location='checkout'">Check out</button></div><div><img class="img1" src="<?=ROOT?>/assets/images/pics/delete.png"></div>
-                    <div style="color:#4DEA25">By Mahinda</div><div></div><div></div><div></div><div style="color:rgba(0, 0, 0, 0.4)">Rs.350</div>
-                </div>
-                <div class="bid2">
-                    <div class="carr"><img class="img3"  src="<?=ROOT?>/assets/images/pics/carrot.jpg"></div><div>Carrot</div><div>10</div><div>KG</div><div class="box20" style="text-align:center;font-size:20px">- 2 +</div><div>Rs.600</div><div><button class="btn2" onclick="document.location='checkout'">Check out</button></div><div><img class="img1" src="<?=ROOT?>/assets/images/pics/delete.png"></div>
-                    <div style="color:#4DEA25">By Sajith</div><div></div><div></div><div></div><div style="color:rgba(0, 0, 0, 0.4)">Rs.350</div>
-                </div>
-                <div class="bid2">
-                    <div class="carr"><img class="img3"  src="<?=ROOT?>/assets/images/pics/carrot.jpg"></div><div>Carrot</div><div>10</div><div>KG</div><div class="box20" style="text-align:center;font-size:20px">- 2 +</div><div>Rs.600</div><div><button class="btn2" onclick="document.location='checkout'">Check out</button></div><div><img class="img1" src="<?=ROOT?>/assets/images/pics/delete.png"></div>
-                    <div style="color:#4DEA25">By Namal</div><div></div><div></div><div></div><div style="color:rgba(0, 0, 0, 0.4)">Rs.350</div>
                 </div>
             </div>
         </div>
-        <div><img class="Line5" style="top:670px" src="<?=ROOT?>/assets/images/pics/Line5.png"></div>
-        <div class="tot">Rs.1850</div>
-
-
-          </div>
-         
-                   
-
+        
     </div>
 
-    
+    <script>
+      // Get all quantity input fields
+const quantityInputs = document.querySelectorAll('.quantity-input');
 
+// Loop through all input fields
+quantityInputs.forEach(input => {
+  // Get the associated plus and minus buttons
+  const plusBtn = input.nextElementSibling;
+  const minusBtn = input.previousElementSibling;
 
-<!-- Modal Section -->
+  // Add click event listener to plus button
+  plusBtn.addEventListener('click', () => {
+    // Increase the quantity by 1
+    input.value = parseInt(input.value) + 1;
+  });
 
-  </body>
+  // Add click event listener to minus button
+  minusBtn.addEventListener('click', () => {
+    // If quantity is already 1, do not decrease it further
+    if (parseInt(input.value) > 1) {
+      // Decrease the quantity by 1
+      input.value = parseInt(input.value) - 1;
+    }
+  });
+});
+
+    </script>
+</body>
 </html>
--- app/view/searchbarbuyer2.php
+++ app/view/searchbarbuyer2.php
@@ -34,8 +34,8 @@ echo "<script>console.log('Filename: ".$filename."');</script>";
 
                 <div class="topnav-options">
                     <!-- <a class="active" href="#home">Home</a> -->
-                    <a href="#orders" id="my-orders">My Orders</a>
-                    <a href="#cart"><i class="fa fa-shopping-cart" style="font-size:18px"></i></a>
+                    <a href="<?=ROOT?>/feed/waiting" id="my-orders">My Orders</a>
+                    <a href="<?=ROOT?>/feed/cart"><i class="fa fa-shopping-cart" style="font-size:18px"></i></a>
                     <a class="notification-icon"><i class="fa fa-bell" style="font-size:18px"></i></a>
                     <span class="notification-count">7</span>
                     <a id="profmyBtn"><img src="<?=ROOT?>/assets/images/Profile_pic/<?=$_SESSION['USER']->image?>" alt=""> </a>
