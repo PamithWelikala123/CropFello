4dbd82fa77df62e0f176abb601fc70250636c2bd
praneeth <praneeth495@gmail.com>
2023-05-12 16:36:13 +0530

    praneeth's commit

--- app/view/cart.view.php
+++ app/view/cart.view.php
@@ -9,14 +9,89 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"-->
-
+    <script data-require="jquery@3.1.1" data-semver="3.1.1" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
     <link rel="stylesheet" href="<?=ROOT?>/assets/css/buyerleftbar.css">
     <link rel="stylesheet" href="<?=ROOT?>/assets/css/buyertopnav.css">
     <link rel="stylesheet" href="<?=ROOT?>/assets/css/cart.css">
+
+    <script type="text/javascript" src="<?php echo AUTO_MAP_URL ?>" defer></script>
+
+
+
+
+    <title>View Post</title>
+
+
+<style>
+    
+    #map {
+           width: 100%;
+           height: 900px;
+           border-radius: 10px;
+           display: none;
+       }
+       .map-container{
+           width: 100%;
+           height: 80%;
+           border-radius: 10px;
+           margin-top: 2%;
+       }
+</style>
+
+
   </head>
-<body>
+<body onload="initMap()">
+
+
+
+<script>
+    
+
+
+let marker;
+function initMap() {
+
+
+                    var start = new google.maps.LatLng(<?php echo json_encode($data['platitude']); ?>,<?php echo json_encode($data['plongitude']); ?>);
+                   var destination=new google.maps.LatLng(<?php echo json_encode($data['sel_lati']); ?>,<?php echo json_encode($data['sel_longi']); ?>);
+
+                    var map = new google.maps.Map(document.getElementById('map'), {
+                    center: start,
+                    zoom: 15,
+                    });
+
+
+                    var directionsService = new google.maps.DirectionsService();
+                    var directionsRenderer = new google.maps.DirectionsRenderer();
+                    directionsRenderer.setMap(map);
+
+                    var request = {
+                    origin: start, 
+                    destination: destination, 
+                    travelMode: 'DRIVING' 
+            };
+
+            
+
+
+            directionsService.route(request, function(result, status) {
+              if (status === 'OK') {
+                directionsRenderer.setDirections(result);
+                var distance = result.routes[0].legs[0].distance.text;
+                var x=distance[0]+distance[1]+distance[2]+distance[3];
+                document.getElementById('distance').value=x;
+                console.log('distance= '+x);
+              }
+            });
+          }
+
+
+
+
+
+</script>
 
 <div class="container">
 
@@ -105,6 +180,7 @@ require APPROOT."/view/searchbarbuyer2.php";
                             <span class="price">Rs. <?php echo($dat2->price);  ?></span>
                           </td>
                           <td><form method="post">
+                            <input type="hidden" name="distance" id="distance" value="">
                             <!--button class="checkout-btn" name="checkout">Checkout</button-->
                             <?php echo '<button class="checkout-btn" name="btn['.$dat2->order_code.']/">Checkout</button>' ?>
                             <?php echo '<button name="del['.$dat2->order_code.']/"><i class="fas fa-trash-alt"></i></button>' ?>
